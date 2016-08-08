1689f1ba20a493be315fda16d5a2b369dd775c67
atungare <ajay.tungare@gmail.com>
2016-08-08 17:55:00 -0400

    render stars

--- src/components/CounterDisplay.js
+++ src/components/CounterDisplay.js
@@ -1,8 +1,17 @@
 const CounterDisplay = React.createClass({
   render() {
+    let counter_val = this.props.counterData.get('value');
+
+    let stars = counter_val > 0 ?
+      Array(counter_val).fill().map((x, i) => {
+        return (<span key={i}>{'*'}</span>);
+      })
+      : [];
+
     return (
       <div>
-        <h3>{ this.props.counterData.get('value') }</h3>
+        <h3>{ counter_val } { stars }
+        </h3>
       </div>
     )
   }
